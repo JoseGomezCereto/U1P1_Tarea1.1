17ab453986a8ebc46faab7dd2f7ebfdcf3254a51
JoseGomezCereto <trabajo.jgomez@gmail.com>
2023-09-25 20:50:54 +0200

    Mejorada la legibilidad y eficiencia usando un Array para los botones de +1 y añadido un método para que estos botones funcionen correctamente.

--- app/src/main/java/com/example/u1p1_tarea11/MainActivity.java
+++ app/src/main/java/com/example/u1p1_tarea11/MainActivity.java
@@ -18,12 +18,18 @@ public class MainActivity extends AppCompatActivity {
     private int counterLower = 0; //Contador inmediatamente inferior al upper(3)
     private int counterBottom = 0; //Contador inferior(4)
 
-    // Constantes para las ID de los botones de reset, las pide haga lo que haga.
+    // Constantes para las ID de los botones de reset.
     private static final int RESET_TOP_BUTTON_ID = R.id.btnResetTop;
     private static final int RESET_UPPER_BUTTON_ID = R.id.btnResetUpper;
     private static final int RESET_LOWER_BUTTON_ID = R.id.btnResetLower;
     private static final int RESET_BOTTOM_BUTTON_ID = R.id.btnResetBottom;
     private static final int RESET_ALL_BUTTON_ID = R.id.btnResetAll;
+
+    // Constantes para las ID de los botones +1.
+    private static final int PLUSONE_TOP_BUTTON_ID = R.id.btnPlusOneTop;
+    private static final int PLUSONE_UPPER_BUTTON_ID = R.id.btnPlusOneUpper;
+    private static final int PLUSONE_LOWER_BUTTON_ID = R.id.btnPlusOneLower;
+    private static final int PLUSONE_BOTTOM_BUTTON_ID = R.id.btnPlusOneBottom;
 
     //Para contabilizar el total de forma correcta necesitamos un método, así podremos llamarlo con cada +1 para que se vaya actualizando.
     private void updateTotal() { //También nos servirá para los reset
@@ -55,7 +61,12 @@ public class MainActivity extends AppCompatActivity {
                 binding.btnResetBottom,
                 binding.btnResetAll
         };
-
+        Button[] plusOneButtons = { //Array para mantener referencias de Buttons+1
+                binding.btnPlusOneTop,
+                binding.btnPlusOneUpper,
+                binding.btnPlusOneLower,
+                binding.btnPlusOneBottom
+        };
         //Inicializamos los componentes básicos
         setSupportActionBar(binding.toolbar); //la toolbar
 
@@ -63,71 +74,67 @@ public class MainActivity extends AppCompatActivity {
         for (TextView textView : counters) {
             textView.setText(getString(R.string.counterDefault));
         }
-
-        //Empezamos con los listeners para los distintos botones. No he visto una forma más eficiente de hacerlo que con un copia-pega.
-        // Boton +1 Top
-        binding.btnPlusOneTop.setOnClickListener(new View.OnClickListener() {
+        //Empezamos con los listeners para los distintos botones.
+        View.OnClickListener plusOne = new View.OnClickListener() {
+            //Es necesaria una supresión de la necesidad de usar constantes, aunque se están usando igual.
+            @SuppressLint("NonConstantResourceId")
             @Override
-            public void onClick(View view) {
-                counterTop++;   // Incrementamos el contador cada click.
-                binding.txtCounterTop.setText(getString(R.string.strCounterTop, counterTop));   //Actualizamos el texto
-                updateTotal();   //Actualizamos el total
+            public void onClick(View v) {  //El método utiliza unos if con las ID de los elementos
+                // Boton +1 Top
+                if (v.getId() == PLUSONE_TOP_BUTTON_ID) {  //comparamos la ID, de ser igual que el botón que estamos pulsando
+                    counterTop++;   // Incrementamos el contador cada click.
+                    binding.txtCounterTop.setText(getString(R.string.strCounterTop, counterTop));   //Actualizamos el texto
+                    updateTotal();   //Actualizamos el total
+                }
+                // Boton +1 Upper
+                if (v.getId() == PLUSONE_UPPER_BUTTON_ID) {
+                    counterUpper++;  // Incrementamos el contador cada click.
+                    binding.txtCounterUpper.setText(getString(R.string.strCounterUpper, counterUpper));   //Actualizamos el texto
+                    updateTotal();   //Actualizamos el total
+                }
+                //Boton +1 Lower
+                if (v.getId() == PLUSONE_LOWER_BUTTON_ID) {
+                    counterLower++;  // Incrementamos el contador cada click.
+                    binding.txtCounterLower.setText(getString(R.string.strCounterLower, counterLower));  //Actualizamos el texto
+                    updateTotal();   //Actualizamos el total
+                }
+                //Boton +1 Bottom
+                if (v.getId() == PLUSONE_BOTTOM_BUTTON_ID) {
+                    counterBottom++;    // Incrementamos el contador cada click.
+                    binding.txtCounterBottom.setText(getString(R.string.strCounterBottom, counterBottom));     //Actualizamos el texto
+                    updateTotal();   //Actualizamos el total
+                }
             }
-        });
-
-        // Boton +1 Upper
-        binding.btnPlusOneUpper.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                counterUpper++;  // Incrementamos el contador cada click.
-                binding.txtCounterUpper.setText(getString(R.string.strCounterUpper, counterUpper));   //Actualizamos el texto
-                updateTotal();   //Actualizamos el total
-            }
-        });
-        //Boton +1 Lower
-        binding.btnPlusOneLower.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                counterLower++;  // Incrementamos el contador cada click.
-                binding.txtCounterLower.setText(getString(R.string.strCounterLower, counterLower));  //Actualizamos el texto
-                updateTotal();   //Actualizamos el total
-            }
-        });
-        //Boton +1 Bottom
-        binding.btnPlusOneBottom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                counterBottom++;    // Incrementamos el contador cada click.
-                binding.txtCounterBottom.setText(getString(R.string.strCounterBottom, counterBottom));     //Actualizamos el texto
-                updateTotal();   //Actualizamos el total
-            }
-        });
-
+        };
         //Creamos un método reset para resetear cada uno de los botones.
         View.OnClickListener reset = new View.OnClickListener() {
             //Es necesaria una supresión de la necesidad de usar constantes, aunque se están usando igual.
             @SuppressLint("NonConstantResourceId")
             @Override
-            public void onClick(View v) {  //El método utiliza unos if con else-if porque no hubo manera
-                //de usar un switch. Ya me hubiera gustado.
+            public void onClick(View v) {  //El método utiliza unos if
+                //Reset Top
                 if (v.getId() == RESET_TOP_BUTTON_ID) {  //comparamos la ID, de ser igual que el botón que estamos pulsando
                     counterTop = 0; //La variable vuelve a cero y más abajo, el texto también vuelve a cero.
                     binding.txtCounterTop.setText(getString(R.string.strCounterTop, counterTop));
                 }
+                //Reset Upper
                 if (v.getId() == RESET_UPPER_BUTTON_ID) {
                     counterUpper = 0;
                     binding.txtCounterUpper.setText(getString(R.string.strCounterUpper, counterUpper));
                 }
+                //Reset Lower
                 if (v.getId() == RESET_LOWER_BUTTON_ID) {
                     counterLower = 0;
                     binding.txtCounterLower.setText(getString(R.string.strCounterLower, counterLower));
                 }
+                //Reset Bottom
                 if (v.getId() == RESET_BOTTOM_BUTTON_ID) {
                     counterBottom = 0;
                     binding.txtCounterBottom.setText(getString(R.string.strCounterBottom, counterBottom));
                 }
-                if (v.getId() == RESET_ALL_BUTTON_ID) { //En el reset total tenemos que
-                    //Resetear toads las variables
+                //Reset All
+                if (v.getId() == RESET_ALL_BUTTON_ID) {
+                    //En el reset total tenemos que resetear toads las variables
                     counterTop = 0;
                     counterUpper = 0;
                     counterLower = 0;
@@ -145,47 +152,9 @@ public class MainActivity extends AppCompatActivity {
         for (Button button : resetButtons) {
             button.setOnClickListener(reset);
         }
+// Y asignamos también el +1 a nuestros botones.
+        for (Button button : plusOneButtons) {
+            button.setOnClickListener(plusOne);
+        }
     }
 }
-
-/*NOTA AL PROFESOR:
-*Intenté hacerlo con un switch, que me gustan bastante, pero me estaba pidiendo
-*todo el rato que las id fueran "constantes" por mucho que las hice constantes,
-* no quiso funcionar. Pego el código aquí, por si tiene solución:
-*
-* // Creamos un OnClickListener para todos los botones de reinicio
-View.OnClickListener reset = new View.OnClickListener() {
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case RESET_TOP_BUTTON_ID:
-                counterTop = 0;
-                binding.txtCounterTop.setText(getString(R.string.strCounterTop, counterTop));
-                break;
-            case RESET_UPPER_BUTTON_ID:
-                counterUpper = 0;
-                binding.txtCounterUpper.setText(getString(R.string.strCounterUpper, counterUpper));
-                break;
-            case RESET_LOWER_BUTTON_ID:
-                counterLower = 0;
-                binding.txtCounterLower.setText(getString(R.string.strCounterLower, counterLower));
-                break;
-            case RESET_BOTTOM_BUTTON_ID:
-                counterBottom = 0;
-                binding.txtCounterBottom.setText(getString(R.string.strCounterBottom, counterBottom));
-                break;
-            case RESET_ALL_BUTTON_ID:
-                counterTop = 0;
-                counterUpper = 0;
-                counterLower = 0;
-                counterBottom = 0;
-                for (TextView textView : counters) {
-                    textView.setText(getString(R.string.counterDefault));
-                }
-                break;
-        }
-        updateTotal();
-    }
-};
-*/
